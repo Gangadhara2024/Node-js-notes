076ff7c51fdc33f7acfecb888d32e80e4dd78568
ruttalagangadhararao224@gmail.com <ruttalagangadhararao224@gmail.com>
2024-10-24 12:52:59 +0530

    nn

--- isAuthMiddleware.js
+++
@@ -1,8 +0,0 @@
-const isAuth = (req, res, next) => {
-  if (req.session.isAuth) {
-    next();
-  } else {
-    return res.send("session expired,please log-in");
-  }
-};
-module.exports = isAuth;
--- nodejs-notes.js
+++ nodejs-notes.js
@@ -367,6 +367,11 @@ app.listen(8000, () => {
  *  5. Server retrieves session: The server uses the session ID to look up the stored session data and maintain user information.
  */
 
+// when, we login with user data same as in DB, it gets login succesful and session is storing user data.
+// session sends response to client side as a cookie as (Session {cookie: { path: '/', _expires: null, originalMaxAge: null, httpOnly: true }}).
+// and when (req.session.isAuth: true), then client know that, user is logged-in user.
+// if we delete sessionID in browser or DB, authorisation will be dummy(i.e user is not logged-in)
+
 // npm i express-session connect-mongodb-session => install packages.
 // express-session:- is middleware for Express.js that allows you to manage sessions for your users.
 // connect-mongodb-session:- is a MongoDB session store for express-session. It stores the session data in a MongoDB database, making it persistent across server restarts.
@@ -426,48 +431,59 @@ app.use(
 // we should create our own api by if condition as method and url.
 
 // @@@@ CRUD(create, read, update, delete) are performed using file system.
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
 
-const server = http.createServer();
+// const server = http.createServer();
 
-server.on("request", (req, res) => {
-  console.log(req.url + " " + req.method);
-  const data = "nodejs11 backend module";
+// server.on("request", (req, res) => {
+//   console.log(req.url + " " + req.method);
+//   const data = "nodejs11 backend module";
 
-  if (req.method === "GET" && req.url === "/writefile") {
-    fs.writeFile("demo.txt", data, (err) => {
-      if (err) throw err;
-      return res.end("write successful");
-    });
-  } else if (req.method === "GET" && req.url === "/appendfile") {
-    fs.appendFile("demo.txt", data, (err) => {
-      if (err) throw err;
-      return res.end("append successful");
-    });
-  } else if (req.method === "GET" && req.url === "/readfile") {
-    fs.readFile("form.html", (err, data) => {
-      if (err) throw err;
-      console.log(data);
-      return res.end(data);
-    });
-  } else if (req.method === "GET" && req.url === "/deletefile") {
-    fs.unlink("demo.txt", (err) => {
-      if (err) throw err;
-      return res.end("deleted file successfully");
-    });
-  } else if (req.method === "GET" && req.url === "/renamefile") {
-    fs.rename("demo.txt", "newDemo.txt", (err) => {
-      if (err) throw err;
-      return res.end("rename successfull");
-    });
-  } else {
-    return res.end(`Api not found : ${req.method} ${req.url} `);
-  }
-});
-server.listen(8000, () => {
-  console.log("http server on... PORT:8000");
-});
+//   if (req.method === "GET" && req.url === "/writefile") {
+//     fs.writeFile("demo.txt", data, (err) => {
+//       if (err) throw err;
+//       return res.end("write successful");
+//     });
+//   } else if (req.method === "GET" && req.url === "/appendfile") {
+//     fs.appendFile("demo.txt", data, (err) => {
+//       if (err) throw err;
+//       return res.end("append successful");
+//     });
+//   } else if (req.method === "GET" && req.url === "/readfile") {
+//     fs.readFile("form.html", (err, data) => {
+//       if (err) throw err;
+//       console.log(data);
+//       return res.end(data);
+//     });
+//   } else if (req.method === "GET" && req.url === "/deletefile") {
+//     fs.unlink("demo.txt", (err) => {
+//       if (err) throw err;
+//       return res.end("deleted file successfully");
+//     });
+//   } else if (req.method === "GET" && req.url === "/renamefile") {
+//     fs.rename("demo.txt", "newDemo.txt", (err) => {
+//       if (err) throw err;
+//       return res.end("rename successfull");
+//     });
+//   } else if (req.url === "/streamread" && req.method === "GET") {
+// const rstream = fs.createReadStream("demo.txt");
+
+// rstream.on("data", (char) => {
+//   console.log(char);
+//   return res.end(char);
+// });
+
+// rstream.on("end", () => {
+//   return res.end();
+// });
+// else {
+//     return res.end(`Api not found : ${req.method} ${req.url} `);
+//   }
+// });
+// server.listen(8000, () => {
+//   console.log("http server on... PORT:8000");
+// });
 // this is how, we can write data into demo.txt file using server.
 // fs.writeFile ==> takes 'file we want to add data', 'data' that we want to add data into file, and callback error.
 
@@ -476,3 +492,49 @@ server.listen(8000, () => {
 // readFile ==> read that file data and in browser it shows in format of <buffer>.
 // unlink ==> will delete the file.
 // rename ==> will delete old file and create new file, and old file data is copied to new file.
+// streamread ==> will readfile without loading file more time. it takes less time to get file data.
+
+//                              @@@@ FILE UPLOADING USING EXPRESS.
+
+// const express = require("express");
+// const fileUpload = require("express-fileupload");
+
+// const app = express();
+
+// app.use(fileUpload());
+
+// app.get("/upload", (req, res) => {
+//   return res.send(`<!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="UTF-8" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <title>Document</title>
+//   </head>
+//   <body>
+//     <form action="/fileupload" method="POST" enctype="multipart/form-data">
+//        Select file to upload:
+//       <input type="file" name="image" /><br>
+//       <button type="submit">submit</button>
+//     </form>
+//   </body>
+// </html>`);
+// });
+
+// app.post("/fileupload", (req, res) => {
+//   console.log(req.files);
+//   const { image } = req.files;
+
+//   if (!image) {
+//     return res.status(400).json("file upload failed");
+//   }
+//   image.mv(__dirname + "/uploads/" + image.name);
+
+//   return res.send("file uploaded succesfully");
+// });
+// app.listen(8000, () => {
+//   console.log("server is on 8000");
+// });
+// // express-fileupload: This middleware simplifies file upload handling. It attaches uploaded files to the req.files object for easier access.
+// // The fileUpload() middleware is registered with Express to handle incoming file uploads. It parses incoming form data that includes files.
+// // .mv() method:- is a function provided by the express-fileupload middleware. It stands for "move" and is used to move the file from its temporary location (where it's uploaded) to a destination of your choice on the server.
